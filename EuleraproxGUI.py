d2852e0356a9c8da97debdf2050af7ca672b175c
Salah Al Shayah <122882654+salahalshayah@users.noreply.github.com>
2023-08-09 17:23:23 +0300

    Update EuleraproxGUI.py

--- EuleraproxGUI.py
+++ EuleraproxGUI.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+
 def solve_ode():
     t0 = float(t0_entry.get())
     y0 = float(y0_entry.get())
@@ -15,27 +16,32 @@ def solve_ode():
     t = t0
     y = y0
 
-    for i in range(int(max_t/h)):
+    for i in range(int(max_t / h)):
         fn = eval(f)
         y = y + h * fn
         t = t + h
-        ts.append(round(t,4))
-        ys.append(round(y,4))
+        ts.append(round(t, 4))
+        ys.append(round(y, 4))
 
     # Update the table
     table.delete(*table.get_children())
     for i in range(len(ts)):
-        table.insert('', i, values=(ts[i], ys[i]))
+        table.insert("", i, values=(ts[i], ys[i]))
 
     # Update the graph
     canvas.figure.clf()
     ax = canvas.figure.add_subplot(111)
-    ax.plot(ts, ys, color='red')
-    ax.set_xlabel('t')
-    ax.set_ylabel('y')
-    ax.set_xlim(min(ts) - (max(ts)-min(ts))/100, max(ts) + (max(ts)-min(ts))/100)
-    ax.set_ylim(min(ys) - (max(ys)-min(ys))/100, max(ys) + (max(ys)-min(ys))/100)
+    ax.plot(ts, ys, color="red")
+    ax.set_xlabel("t")
+    ax.set_ylabel("y")
+    ax.set_xlim(
+        min(ts) - (max(ts) - min(ts)) / 100, max(ts) + (max(ts) - min(ts)) / 100
+    )
+    ax.set_ylim(
+        min(ys) - (max(ys) - min(ys)) / 100, max(ys) + (max(ys) - min(ys)) / 100
+    )
     canvas.draw()
+
 
 # Create the main window
 root = tk.Tk()
@@ -75,15 +81,15 @@ solve_button.grid(row=5, column=0)
 table_frame = ttk.Frame(root)
 table_frame.grid(row=6, column=0, columnspan=2)
 
-table = ttk.Treeview(table_frame, columns=('t', 'y'), show='headings')
-table.column('t', width=200, anchor='center')
-table.column('y', width=200, anchor='center')
-table.heading('t', text='t')
-table.heading('y', text='y')
-table.pack(side='left', fill='both', expand=True)
+table = ttk.Treeview(table_frame, columns=("t", "y"), show="headings")
+table.column("t", width=200, anchor="center")
+table.column("y", width=200, anchor="center")
+table.heading("t", text="t")
+table.heading("y", text="y")
+table.pack(side="left", fill="both", expand=True)
 
-scrollbar = ttk.Scrollbar(table_frame, orient='vertical', command=table.yview)
-scrollbar.pack(side='right', fill='y')
+scrollbar = ttk.Scrollbar(table_frame, orient="vertical", command=table.yview)
+scrollbar.pack(side="right", fill="y")
 table.configure(yscrollcommand=scrollbar.set)
 
 # Create the graph area
